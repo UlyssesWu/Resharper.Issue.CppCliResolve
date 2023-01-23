ecb31f8f18f04ad8cbef0e189e736c7bac64347d
Ulysses Wu <wdwxy12345@gmail.com>
2023-01-24 00:25:10 +0800

    init

---
+++ Resharper.Issue.CppCliResolve/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FreeMote;
+
+namespace Resharper.Issue.CppCliResolve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The following line is red if ReSharper enabled.");
+            var emote = new Emote(IntPtr.Zero, 100, 100);
+            emote.ChallengeResharper();
+            Console.WriteLine("BuIt builds without error.");
+            Console.ReadLine();
+        }
+    }
+}
